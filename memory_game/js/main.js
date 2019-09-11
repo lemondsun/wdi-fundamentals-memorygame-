d792b0bcdb5f9ccd4f90ee6309a1d5c8a99d0893
Jason Mullings <wjasonmullings@gmail.com>
2019-09-11 01:53:54 -0400

    cleaned game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,16 +1,21 @@
-console.log("Up and running!")
-let cards = ["queen","queen","king","king"];
+
+let cards = ["queen", "queen", "king", "king"];
 let cardsInPlay = [];
-let cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardsInPlay);
-cardTwo = cards[1];
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardsInPlay[1]);
-if (cardsInPlay.length===2 && cardsInPlay[0]=== cardsInPlay[1]) {
-    console.log("user picked twice")
-    alert("You found a match!")
-}else if (cardsInPlay.length===2&&cardsInPlay[0]!==cardsInPlay[1]) {
-    console.log("user picked twice")
-    alert("sorry, try again.")
+function checkForMatch(){
+    if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
+        //will change console.log to alert in final itteration
+        console.log("You found a match!")
+    } else if (cardsInPlay.length === 2 && cardsInPlay[0] !== cardsInPlay[1]) {
+        //will change console.log to alert in final itteration
+        console.log("sorry, try again.")
+    }
+
 }
+function flipCard(cardId) {
+    console.log("User flipped "+ cards[cardId]);
+    cardsInPlay.push(cards[cardId]);
+    checkForMatch();
+
+}
+flipCard(0)
+flipCard(2)
