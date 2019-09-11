c6bd9bf33cd18473e7161fa366326cee5ff0176e
Jason Mullings <wjasonmullings@gmail.com>
2019-09-10 20:11:46 -0400

    Created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,16 @@
 console.log("Up and running!")
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
-console.log("User flipped "+ cardOne);
-console.log("User flipped " + cardThree);
+let cards = ["queen","queen","king","king"];
+let cardsInPlay = [];
+let cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log("User flipped " + cardsInPlay);
+cardTwo = cards[1];
+cardsInPlay.push(cardTwo);
+console.log("User flipped " + cardsInPlay[1]);
+if (cardsInPlay.length===2 && cardsInPlay[0]=== cardsInPlay[1]) {
+    console.log("user picked twice")
+    alert("You found a match!")
+}else if (cardsInPlay.length===2&&cardsInPlay[0]!==cardsInPlay[1]) {
+    console.log("user picked twice")
+    alert("sorry, try again.")
+}
